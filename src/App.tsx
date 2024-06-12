428466411255f064f3e8f92dd8d097639eb8523d
Dmitriy <fstpain@gmail.com>
2024-06-12 22:55:03 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {BrowserRouter, NavLink, Route, Routes, Navigate} from "react-router-dom";
+import {Error404} from "./components/pages/Error404";
 
 
 function App() {
@@ -8,10 +13,18 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    Здесь будет навигация
+                    <NavLink to={'/1'}><span>asdada</span></NavLink>
                 </div>
                 <div className={styles.content}>
-                    Здесь будут кроссовки
+
+                        <Routes>
+                            <Route path={"/"}  element={<Navigate to={'/1'}/>} ></Route>
+                            <Route path={"/1"} element={<PageOne/>}></Route>
+                            <Route path={"/2"} element={<PageTwo/>}></Route>
+                            <Route path={"/3"} element={<PageThree/>}></Route>
+                            <Route path={"*"} element={<Error404/>}></Route>
+                        </Routes>
+
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
