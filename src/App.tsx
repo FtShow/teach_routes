efaaccbef0d9823b560e9a5f89f9b2a2942d2e11
Dmitriy <fstpain@gmail.com>
2024-06-16 13:16:52 +0300

    add page model

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,42 @@
-import React, {useState} from 'react';
-import styles from "./components/Site.module.css";
-import {PageOne} from "./components/pages/PageOne";
-import {PageTwo} from "./components/pages/PageTwo";
-import {PageThree} from "./components/pages/PageThree";
-import {BrowserRouter, NavLink, Route, Routes, Navigate, Link} from "react-router-dom";
+import React from 'react';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from "./components/pages/Error404";
-import S from "./Navstyles.module.css"
+import {Adidas} from "./components/pages/Adidas";
+import styles from "./components/Site.module.css";
+import {S} from './components/pages/_styles';
+import {Puma} from "./components/pages/Puma";
+import {Abibas} from "./components/pages/Abibas";
+import {Model} from "./components/pages/Model";
 
-import styled from "styled-components";
-const NavWrapper = styled.div`
-margin-left: 10px;
-  font-size: 20px;
-  & > a {
-    text-decoration: none;
-    color: #282c34;
-  }
-  & > a.active{
-    color: #7fb8f1;
-    text-decoration: underline;
-  }
-`
 
 const PATH = {
-    PAGE1:'/1',
-    PAGE2:'/2',
-    PAGE3:'/3',
-} as const
-
-
+    PAGE1: '/adidas',
+    PAGE2: '/puma',
+    PAGE3: '/abibas',
+} as const;
 
 function App() {
-    const NavActiveClass = ({isActive}: any) =>
-        isActive ? S.activeNavLink : S.navLink
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <NavWrapper><NavLink to={PATH.PAGE1} >PAGE ONE</NavLink></NavWrapper>
-                    <NavWrapper><NavLink to={PATH.PAGE2} >PAGE TWO</NavLink></NavWrapper>
-                    <NavWrapper> <NavLink to={PATH.PAGE3} >PAGE THREE</NavLink></NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
+                    <a href="page3">page3 HTML</a>
                 </div>
                 <div className={styles.content}>
+                    <Routes>
+                        <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
 
-                        <Routes>
-                            <Route path={"/"}  element={<Navigate to={'/1'}/>} ></Route>
-                            <Route path={"/1"} element={<PageOne/>}></Route>
-                            <Route path={"/2"} element={<PageTwo/>}></Route>
-                            <Route path={"/3"} element={<PageThree/>}></Route>
-                            <Route path={"*"} element={<Error404/>}></Route>
-                        </Routes>
+                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
+                        <Route path={PATH.PAGE2} element={<Puma/>}/>
+                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
+                        <Route path={'/adidas/:id'} element={<Model/>}/>
 
+                        <Route path={'/*'} element={<Error404/>}/>
+                    </Routes>
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
@@ -58,5 +44,5 @@ function App() {
     );
 }
 
-
 export default App;
+
