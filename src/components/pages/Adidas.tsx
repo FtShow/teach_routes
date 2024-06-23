fedc6453ae07fdeeaae5e11074edd5e32c87928c
Dmitriy <fstpain@gmail.com>
2024-06-23 11:35:42 +0300

    add route

--- src/components/pages/Adidas.tsx
+++ src/components/pages/Adidas.tsx
@@ -3,18 +3,18 @@ import adidasModel1 from './../../assets/adidas/AdiFOM_TRXN_Shoes_Black_IG7453_0
 import adidasModel2 from './../../assets/adidas/Superstar_XLG_Shoes_Black_IG9777_01_standard.webp'
 import adidasModel3
     from './../../assets/adidas/PostMove_Mid_Cloudfoam_Super_Lifestyle_Basketball_Mid_Classic_Shoes_Black_GY7163_01_standard.webp'
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 
-export type ItemType = {
-    id: number,
+export type AdidasItem = {
+    id:number;
     model: string;
     collection: string;
     price: string;
     picture: string;
 }
-export const adidasArr: ItemType[] = [
+export const adidasArr: AdidasItem[] = [
     {
-        id: 1,
+        id:1,
         model: 'ADIDAS ADIFOM TRXN',
         collection: 'new collection1',
         price: '100200$',
@@ -22,32 +22,35 @@ export const adidasArr: ItemType[] = [
 
     },
     {
-        id: 2,
+        id:2,
         model: 'ADIDAS ADIFOM SUPER',
         collection: 'new collection22',
         price: '200300$',
         picture: adidasModel2
     },
     {
-        id: 3,
+        id:3,
         model: 'ADIDAS SUPER SUPERSKI',
         collection: 'new collection333',
         price: '300400$',
         picture: adidasModel3
     }
 ]
-
 export const Adidas = () => {
     return (
         <div>
-            <h2 style={{textAlign: 'center'}}> ADIDAS</h2>
-
-            {adidasArr.map((el)=><NavLink key={el.id} to={`/adidas/${el.id}`}><img
-                src={el.picture}
-                alt={el.model}
-                style={{width: "200px", height: "auto", margin: "10px"}}
-            /></NavLink>)}
-
+            <h2> ADIDAS</h2>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                {adidasArr.map((adidas, index) => (
+                    <Link key={adidas.id} to={`/adidas/${adidas.id}`}>
+                        <img
+                            src={adidas.picture}
+                            alt={adidas.model}
+                            style={{width: '200px', height: 'auto', marginRight: '10px'}}
+                        />
+                    </Link>
+                ))}
+            </div>
             <p>
                 What is Lorem Ipsum?
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
@@ -89,4 +92,3 @@ export const Adidas = () => {
         </div>
     );
 };
-
--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,34 +1,53 @@
-import React from 'react';
-import {adidasArr, ItemType} from "./Adidas";
+import {adidasArr, AdidasItem} from "./Adidas";
 import {useParams} from "react-router-dom";
-import {Error404} from "./Error404";
-import {pumaArr} from "./Puma";
+import {pumaArr, PumaItem} from "./Puma";
 
-export type crossModelType = {
-    adidas: ItemType[],
-    puma: ItemType[]
+
+
+type CrossModels= {
+    [key: string]: (AdidasItem[] | PumaItem[]);
 }
 
-
-const crossModel = {
-    adidas: adidasArr,
+const crossModels: CrossModels={
+    adidas:adidasArr,
     puma: pumaArr
 }
 
 export const Model = () => {
-    const { model, id } = useParams<{ model: keyof crossModelType; id: string }>();
-    const currentModel = model ? crossModel[model] : null
-    const arr = currentModel?.find((el: ItemType)=> el.id === Number(id))
-   // console.log(arr)
-    return (
-        <div>
-            {arr && <>
-            <h3>{arr.model}</h3>
-            <span>{arr.collection}</span>
-            <span>{arr.price}</span>
-            <img src={arr.picture}/></> || <Error404/>}
+    // const params=useParams()
+    const { model, id } = useParams();
+    console.log(model)
+        //const currentModel=adidasArr.find(el=>el.id===Number(id))
+    const currentModel = model
+        ? crossModels[model].find((el) => el.id === Number(id))
+        : null;
+
+      return (
+        <div style={{textAlign:'center'}}>
+            {currentModel
+                ?<>
+                    <h2>{currentModel?.model}</h2>
+                    <h4>{currentModel?.collection}</h4>
+                    <h3>{currentModel?.price}</h3>
+                    <img
+                        src={currentModel?.picture}
+                        alt={currentModel?.model}
+                        style={{width: '600px', height: 'auto', marginRight: '10px'}}
+                    />
+                </>
+                :   <h2>Модель отсутствует</h2>
+            }
 
 
+
+        {/*    <h2>{adidasArr[Number(params.id)].model}</h2>*/}
+        {/*    <h4>{adidasArr[Number(params.id)].collection}</h4>*/}
+        {/*    <h3>{adidasArr[Number(params.id)].price}</h3>*/}
+        {/*    <img*/}
+        {/*        src={adidasArr[Number(params.id)].picture}*/}
+        {/*        alt={adidasArr[Number(params.id)].model}*/}
+        {/*        style={{width: '600px', height: 'auto', marginRight: '10px'}}*/}
+        {/*    />*/}
         </div>
-    );
+);
 };
