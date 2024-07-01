8d5345de12e557b3f2c54f921437f0f128e65450
Dmitriy <fstpain@gmail.com>
2024-07-01 13:57:43 +0300

    rrd 6.4

---
+++ src/routes/router.tsx
@@ -0,0 +1,43 @@
+import {createBrowserRouter, Link} from "react-router-dom";
+import App from "../App";
+import {Error404} from "../components/pages/Error404";
+import {Adidas} from "../components/pages/Adidas";
+import {Prices} from "../components/pages/Prices";
+import {Abibas} from "../components/pages/Abibas";
+import {Puma} from "../components/pages/Puma";
+
+const PATH = {
+    ADIDAS: '/adidas',
+    PUMA: '/puma',
+    ABIBAS: '/abibas',
+    PRICES: '/prices',
+
+} as const
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <Error404/>,
+        children:[
+            {
+                path: PATH.ADIDAS,
+                element: <Adidas/>
+            },
+            {
+                path: PATH.PUMA,
+                element: <Puma/>
+            },
+            {
+                path: PATH.ABIBAS,
+                element: <Abibas/>
+            },
+            {
+                path: PATH.PRICES,
+                element: <Prices/>
+            },
+        ]
+    },
+
+]);
+
